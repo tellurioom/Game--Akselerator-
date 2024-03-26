87c54ca3225199c296942fffb9e7f685553c5a59
Tellurioom <@Tellurioom>
2024-03-27 00:01:15 +0700

    .

--- code/3.py
+++ code/3.py
@@ -8,19 +8,14 @@ FPS = 24
 
 tile_file = '../source/tmw_desert_spacing.png'
 csv_file = '../source/desert.csv'
-image = pygame.image.load(tile_file)
-rect = image.get_rect()
-
-pygame.init()
-display = pygame.display.set_mode(rect.size)
-clock = pygame.time.Clock()
 
 
 class Tiles(pygame.sprite.Sprite):
-    def __init__(self, file_path, csv_file, size=(32, 32)):
+    def __init__(self, file_path, size=(32, 32)):
         super().__init__()
         self.tiles = []
         self.tiles_map = pygame.sprite.Group()
+        self.map_size = (0, 0)
         self.size = size
         self.image = pygame.image.load(file_path)
         self.max_size = self.image.get_size()
@@ -32,25 +27,45 @@ class Tiles(pygame.sprite.Sprite):
                 self.tile = pygame.Surface(self.size)
                 self.tile.blit(self.image, (0, 0), (x, y, self.size[0], self.size[1]))
                 self.tiles.append(self.tile)
-        print(self.tiles)
 
-    def draw_tilemap(self):
+    def draw_tilemap(self, display, csv_file):
         with open(csv_file, newline='') as csvfile:
             read = csv.reader(csvfile, delimiter=',')
+            x = 0
+            y = 0
             for row in read:
+                x = 0
                 for tile in row:
-                    print(tile)
-                    sprite = self.tiles[int(tile)]
-                    self.tiles_map.add(sprite)
+                    display.blit(self.tiles[int(tile)], (x, y))
+                    x += self.size[0]
+                y += self.size[1]
+            self.map_size = (x * self.size[0], y * self.size[1])
+        return self.map_size
+
+    def get_map_size(self):
+        with open(csv_file, newline='') as csvfile:
+            read = csv.reader(csvfile, delimiter=',')
+            x = 0
+            y = 0
+            for row in read:
+                x = 0
+                for tile in row:
+                    x += self.size[0]
+                y += self.size[1]
+            self.map_size = (x, y)
+        return self.map_size
 
 
-first = Tiles(tile_file, csv_file)
-first.load_tiles()
-img = first.tiles
-first.draw_tilemap()
+pygame.init()
 
-all_sprites = pygame.sprite.Group()
-all_sprites.add(first.tiles_map)
+
+map = Tiles(tile_file)
+map.load_tiles()
+
+
+display = pygame.display.set_mode(map.get_map_size())
+clock = pygame.time.Clock()
+
 
 running = True
 while running:
@@ -61,7 +76,7 @@ while running:
 
     display.fill(BACKGROUND_COLOR)
 
-    all_sprites.draw(display)
+    map.draw_tilemap(display, csv_file)
 
     pygame.display.update()
 
