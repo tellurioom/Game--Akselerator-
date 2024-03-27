c5fbe5c32085f5b292ed51c893d5f4fb4c469f22
Tellurioom <@Tellurioom>
2024-03-27 22:43:26 +0700

    .

--- code/Tile_map.py
+++ code/Tile_map.py
@@ -1,18 +1,20 @@
 import csv
 import pygame
-import os
 
 SIZE = WIDTH, HEIGHT = 600, 400
 BACKGROUND_COLOR = (255, 255, 255)
 FPS = 24
 
-tile_file = '../source/tmw_desert_spacing.png'
+tiles_file = '../source/tmw_desert_spacing.png'
 csv_file = '../source/desert.csv'
 
 
-class Tiles(pygame.sprite.Sprite):
-    def __init__(self, file_path, size=(32, 32)):
-        super().__init__()
+class Tiles:
+    def __init__(self, file_path: str, size=(32, 32)):
+
+        # класс по загрузке тайлов
+        # принимает: путь к файлу с тайлами, размеры одного тайла
+
         self.tiles = []
         self.tiles_map = pygame.sprite.Group()
         self.map_size = (0, 0)
@@ -21,6 +23,7 @@ class Tiles(pygame.sprite.Sprite):
         self.max_size = self.image.get_size()
         self.tile = pygame.Surface(self.size)
 
+    # функция по загрузке тайлов из файла
     def load_tiles(self):
         for y in range(1, self.max_size[1], self.size[1]+1):
             for x in range(1, self.max_size[0], self.size[0]+1):
@@ -28,7 +31,33 @@ class Tiles(pygame.sprite.Sprite):
                 self.tile.blit(self.image, (0, 0), (x, y, self.size[0], self.size[1]))
                 self.tiles.append(self.tile)
 
-    def draw_tilemap(self, display, csv_file):
+    def get_map_size(self):
+
+        # функция по получению размеров итоговой карты
+        # возвращает итоговый размер карты
+        # вызвать в методе display.set_mode()
+
+        with open(csv_file, newline='') as csvfile:
+            read = csv.reader(csvfile, delimiter=',')
+            x = 0
+            y = 0
+            for row in read:
+                x = 0
+                for _ in row:
+                    x += self.size[0]
+                y += self.size[1]
+            self.map_size = (x, y)
+        return self.map_size
+
+    def draw_tilemap(self, display: pygame.display, csv_file: str):
+
+        # функция по отрисовки карты на экран
+        # вызывать в основном цикле игры
+        # принимает: surface="экран", путь к файлу *.csv
+
+        if csv_file[len(csv_file)-4:len(csv_file)] != '.csv':
+            raise NameError(f'{csv_file} не является файлом с расширением *.csv')
+
         with open(csv_file, newline='') as csvfile:
             read = csv.reader(csvfile, delimiter=',')
             x = 0
@@ -40,28 +69,12 @@ class Tiles(pygame.sprite.Sprite):
                     x += self.size[0]
                 y += self.size[1]
             self.map_size = (x * self.size[0], y * self.size[1])
-        return self.map_size
-
-    def get_map_size(self):
-        with open(csv_file, newline='') as csvfile:
-            read = csv.reader(csvfile, delimiter=',')
-            x = 0
-            y = 0
-            for row in read:
-                x = 0
-                for tile in row:
-                    x += self.size[0]
-                y += self.size[1]
-            self.map_size = (x, y)
-        return self.map_size
 
 
 pygame.init()
 
-
-map = Tiles(tile_file)
+map = Tiles(tiles_file)
 map.load_tiles()
-
 
 display = pygame.display.set_mode(map.get_map_size())
 clock = pygame.time.Clock()
@@ -73,11 +86,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     display.fill(BACKGROUND_COLOR)
 
     map.draw_tilemap(display, csv_file)
 
     pygame.display.update()
-
 pygame.quit()
